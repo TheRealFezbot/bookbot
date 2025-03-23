de04da443cbd5ce0e57462f08f8fe7511afa1a2a
TheRealFezbot <FerryRietveld@live.nl>
2025-03-23 19:52:08 +0200

    G: added functionality to print up a report,  tidied up code

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-    return file_contents
 
-from stats import num_of_words
-from stats import number_of_characters
+# import stats.py
+from stats import get_book_text, num_of_words, number_of_characters, sort_character_count, print_report
 
-num_of_words(get_book_text("books/frankenstein.txt"))
-number_of_characters(get_book_text("books/frankenstein.txt"))
+# variables
+path = "books/frankenstein.txt"
+word_count = num_of_words(get_book_text(path))
+char_counts = number_of_characters(get_book_text(path))
+sorted_chars = sort_character_count(char_counts)
 
-# def main():
-#     print(get_book_text("books/frankenstein.txt"))
-    
-
-# main()
+#print the report
+print_report(path, word_count, sorted_chars)
--- stats.py
+++ stats.py
@@ -1,10 +1,51 @@
+# get the book text from the path
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+#count the number of words
 def num_of_words(contents):
     num_words = len(contents.split())
-    print(f"{num_words} words found in the document")
+    return num_words
 
+#count the number of characters
 def number_of_characters(contents):
     contents = contents.lower()
     dictionary = {}
     for character in contents:
         dictionary[character] = dictionary.get(character, 0) + 1
-    print(dictionary)
+    return(dictionary)
+
+#sort the characters
+def sort_character_count(char_dict):
+    chars_list = []
+    for char, count in char_dict.items():
+        chars_list.append({"char": char, "count": count})
+
+    def sort_on(dict_item):
+        return dict_item["count"]
+    
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
+
+#print the report 
+def print_report(path, word_count, sorted_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        count = char_dict["count"]
+        if char.isalpha():
+            print(f"{char}: {count}")
+    
+    print("============= END ===============")
+
+
+
+
+
