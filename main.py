7000e5230394358a98c9ad58ee5e44f78c4a3221
TheRealFezbot <ferryrietveld@live.nl>
2025-11-28 17:31:47 +0200

    Added stats.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from stats import get_num_words
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+main()
